2cb0067d3ef4c3db13dfa37976919d08f2f9f3bd
David <deividgf567@mail.com>
2023-03-22 11:12:33 +0100

    commit 4

--- functions.py
+++ functions.py
@@ -1,6 +1,66 @@
 import csv
+import math
 #type,fixed acidity,volatile acidity,citric acid,residual sugar,chlorides,free sulfur dioxide,total sulfur dioxide,density,pH,sulphates,alcohol,quality
 
+
+
+def split(diccionario):
+    #Dos diccionarios vacios para guardar los datos
+    diccionarioWhite = {}
+    diccionarioRed = {}
+
+    #Se recorre el diccionario
+    for type, row in diccionario.items():
+
+        #Si el atributo type es "white", se añade al diccionarioWhite
+        if(type == "White"):
+            print("Blanco")
+            diccionarioWhite.append(diccionario[row])
+
+        #Si el atributo type es "red", se añade al diccionarioRed
+        elif(type == "red"):
+            print("Rojo")
+            diccionarioRed.append(diccionario[row])
+
+    return diccionarioRed, diccionarioWhite
+
+
+
+def reduce(diccionario, atributo):
+    #lista vacia para guardar los datos
+    listaValores = []
+
+    #Se recorre el diccionario
+    for type, row in diccionario.items():
+        #Si la clave coincide con el atributo que se pasa por parámetro, se añade a la lista su valor
+        if type == atributo:
+            listaValores.append(row)
+            
+    return listaValores
+
+
+def silhouette(lista1, lista2):
+    #lista que guardará los valores de S(i) para hacer la posterior media
+    listaS = []
+    maximoLista1 = max(lista1)
+    maximoLista2 = max(lista2)
+
+    listaDistanciaA = []
+    listaDistanciaB = []
+
+    #Calcular b(i) -> Distancia lista1(i) y lista2
+    for i in lista1:
+        for j in lista2:
+            aux = i - j
+            distancia = math.sqrt(math.abs(math.pow(aux, 2)))
+            distancia.append(listaDistanciaB)
+
+    #Calcular a(i) -> Distancia lista1(i), resto lista1
+    valorIni = lista1[0]
+    for valor in lista1:
+
+
+'''
 def read_data(nombreFichero):
     atributos = ["type","fixed" "acidity","volatile" "acidity","citric acid","residual sugar","chlorides","free sulfur dioxide","total sulfur dioxide","density","pH","sulphates","alcohol","quality"]
     dict = []
@@ -19,18 +79,4 @@ def read_data(nombreFichero):
                 seating.append(row)
         seating = [ {letter: None for letter in seats} for i in range(self.__num_rows)]
 
-        #seating = [ {letter: None for letter in seats} for i in range(self.__num_rows)]
-
-
-def split(diccionario):
-    diccionarioWhite = {}
-    diccionarioRed = {}
-
-    for type, row in diccionario.items():
-        if(type == "White"):
-            print("Blanco")
-            diccionarioWhite.append(diccionario[row])
-
-        elif(type == "red"):
-            print("Rojo")
-            diccionarioRed.append(diccionario[row])
+        #seating = [ {letter: None for letter in seats} for i in range(self.__num_rows)]'''
