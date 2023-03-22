a08c397478eddb38414b94f6d17c5b52411c8f61
David <deividgf567@mail.com>
2023-03-22 10:31:19 +0100

    commit 2

--- functions.py
+++ functions.py
@@ -1,12 +1,7 @@
+import csv
+
 def read_data(nombreFichero):
-    linea = 1
-    f = open(nombreFichero, mode="rt", encoding="utf-8")
-
-    #leemos el fichero
-    atributos = f.readline()
-    #print("Leemos la primera linea: " + atributos)
-
-    for linea in f:
-        print(linea)
-
-    f.close()
+    with open(nombreFichero, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
